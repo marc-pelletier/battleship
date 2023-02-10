2b1cac078fddb8de1baa77eea27b45e993225e5e
Steve <marlumpy@gmail.com>
2023-02-10 14:59:39 -0500

    First commit - contains html and css

---
+++ js/main.js
@@ -0,0 +1,13 @@
+let boards = document.querySelectorAll(".row");
+
+/*----- classes -----*/
+
+/*----- constants -----*/
+
+/*----- app's state (variables) -----*/
+
+/*----- cached element references -----*/
+
+/*----- event listeners -----*/
+
+/*----- functions -----*/
