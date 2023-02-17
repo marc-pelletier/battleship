9a5371eb5b038bf2b04f5414147419e37bb6c6cd
Steve <marlumpy@gmail.com>
2023-02-17 10:06:29 -0500

    fix rendering bug

--- js/main.js
+++ js/main.js
@@ -384,6 +384,7 @@ function play() {
         winscreenEl.classList.remove("hidden");
         killBoardEvents(playerBoardEl);
         killBoardEvents(enemyBoardEl);
+        swapPlayers();
         renderCells(blue.board, "blue", false);
     }
     else if (!blue.shipHealth.reduce((acc,s) => acc + s, 0)) {
@@ -392,7 +393,8 @@ function play() {
         winscreenEl.classList.remove("hidden");
         killBoardEvents(playerBoardEl)
         killBoardEvents(enemyBoardEl)
-        renderCells(blue.board, "blue", false);
+        swapPlayers();
+        renderCells(red.board, "red", false);
     }
 
     //AI plays
@@ -649,6 +651,9 @@ function renderCells(board, playerName, hiddenEls) {
         for (let x=0; x<10; x++) {
             let cellEl = document.getElementById(`${x}-${y} ${playerName}`);
             let cell = board[x][y];
+            if (enemy.board[x][y].hasShip && enemy.shipHealth[enemy.board[x][y].shipIndex] == 0 && selectedPlayer == currentPlayer && !hiddenEls && selectedPlayer != "0") {
+                renderShips(document.getElementById(`${x}-${y} ${playerName=="blue"?"red":"blue"}`),enemy.board[x][y]);
+            }
             if (board[x][y].hasShip && board[x][y].isHit) {
                 cellEl.innerHTML = '<img src="images/hit.png">';
             }
@@ -661,9 +666,6 @@ function renderCells(board, playerName, hiddenEls) {
             else {
                 cellEl.innerHTML = '';
                 cellEl.style.backgroundImage = '';
-            }
-            if (enemy.board[x][y].hasShip && enemy.shipHealth[enemy.board[x][y].shipIndex] == 0 && selectedPlayer == currentPlayer && !hiddenEls) {
-                renderShips(document.getElementById(`${x}-${y} ${playerName=="blue"?"red":"blue"}`),enemy.board[x][y]);
             }
         }
     }
